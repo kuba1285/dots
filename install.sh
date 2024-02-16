89b501aaa70c7a45874ada0feb2dfc62964494cd
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-16 21:18:31 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -243,8 +243,8 @@ if [[ $INST == "Y" || $INST == "y" ]]; then
         sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
         sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
         echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
-    fi
-
+        echo -e "WLR_NO_HARDWARE_CURSORS=1" | sudo tee -a /etc/environment
+        
     # Install the correct hyprland version
     echo -e "$CNT - Installing Hyprland, this may take a while..."
     if [[ "$ISNVIDIA" == true ]]; then
