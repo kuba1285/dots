485c028cc1d6b8b62b3e8af16836758a0613c407
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-22 01:46:48 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-BIN=$(cd $(dirname $0); pwd)
-PARENT=$(cd $(dirname $0)/../; pwd)
-
 # set some colors
 CNT="[\e[1;36mNOTE\e[0m]"
 COK="[\e[1;32mOK\e[0m]"
@@ -10,12 +7,14 @@ CER="[\e[1;31mERROR\e[0m]"
 CAT="[\e[1;37mATTENTION\e[0m]"
 CWR="[\e[1;35mWARNING\e[0m]"
 CAC="[\e[1;33mACTION\e[0m]"
-INSTLOG="install.log"
 
-# Define the software that would be inbstalled 
+# Define variables
+BIN=$(cd $(dirname $0); pwd)
+PARENT=$(cd $(dirname $0)/../; pwd)
+INSTLOG="install.log"
 LISTAPP="$BIN/list-app"
-LISTNVIDIA="$BIN/list-nvidia"
 LISTCUSTOM="$BIN/list-custom"
+LISTNVIDIA="$BIN/list-nvidia"
 ######
 
 # function that would show a progress bar to the user
@@ -74,13 +73,6 @@ else
     exit
 fi
 
-# find the Nvidia GPU
-if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
-    ISNVIDIA=true
-else
-    ISNVIDIA=false
-fi
-
 ### Disable wifi powersave mode ###
 read -rep $'[\e[1;33mACTION\e[0m] - Would you like to disable WiFi powersave? (y,n) ' WIFI
 if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
@@ -128,51 +120,44 @@ fi
 ### Install listed pacakges ####
 read -rep $'[\e[1;33mACTION\e[0m] - Would you like to install the packages? (y,n) ' INST
 if [[ $INST == "Y" || $INST == "y" ]]; then
-
     echo -e "$CNT - Installing needed components, this may take a while..."
     install_list $LISTAPP
-
-    # Setup Nvidia if it was found
-    if [[ "$ISNVIDIA" == true ]]; then
-        echo -e "$CNT - Nvidia GPU support setup stage, this may take a while..."
-        install_list $LISTNVIDIA
-    
-        # update config
-        sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-        sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-        echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
-        echo -e "WLR_NO_HARDWARE_CURSORS=1" | sudo tee -a /etc/environment
-        
-        # Install the correct hyprland version
-        echo -e "$CNT - Installing Hyprland, this may take a while..."
-        
-        #check for hyprland and remove it so the -nvidia package can be installed
-        if yay -Q hyprland &>> /dev/null ; then
-            yay -R --noconfirm hyprland &>> $INSTLOG &
-        fi
-        install_software hyprland-nvidia
-    else
-        install_software hyprland
-    fi
-
-    # Start the bluetooth service
-    echo -e "$CNT - Starting the Bluetooth Service..."
-    sudo systemctl enable --now bluetooth.service &>> $INSTLOG
-    sleep 2
-
-    # Enable the sddm login manager service
-    echo -e "$CNT - Enabling the SDDM Service..."
-    sudo systemctl enable sddm &>> $INSTLOG
-    sleep 2
-    
-    # Clean out other portals
-    echo -e "$CNT - Cleaning out conflicting xdg portals..."
-    yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk &>> $INSTLOG
 fi
 
 read -rep $'[\e[1;33mACTION\e[0m] - Would you like to install custom applications from a list? (y,n) ' CUSTOM_APPS
 if [[ $CUSTOM_APPS == "Y" || $CUSTOM_APPS == "y" ]]; then
     install_list $LISTCUSTOM 
+fi
+
+# find the Nvidia GPU
+if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
+    ISNVIDIA=true
+else
+    ISNVIDIA=false
+fi
+
+# Setup Nvidia if it was found
+if [[ "$ISNVIDIA" == true ]]; then
+    echo -e "$CNT - Nvidia GPU support setup stage, this may take a while..."
+    install_list $LISTNVIDIA
+
+    # update config
+    sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+    sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
+    echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
+    echo -e "WLR_NO_HARDWARE_CURSORS=1" | sudo tee -a /etc/environment
+    echo -e "\nsource = ~/.config/hypr/nvidia.conf" >> ~/.config/hypr/hyprland.conf
+
+    # Install the correct hyprland version
+    echo -e "$CNT - Installing Hyprland, this may take a while..."
+
+    #check for hyprland and remove it so the -nvidia package can be installed
+    if yay -Q hyprland &>> /dev/null ; then
+        yay -R --noconfirm hyprland &>> $INSTLOG &
+        install_software hyprland-nvidia
+    else
+        install_software hyprland
+    fi
 fi
 
 ### Install MBP audio driver ###
@@ -193,63 +178,62 @@ if [[ $CFG == "Y" || $CFG == "y" ]]; then
     cp -rT $PARENT/. ~/ &>> $INSTLOG
     echo -e '\neval "$(starship init bash)"' >> ~/.bashrc
     echo -e '\neval "$(starship init zsh)"' >> ~/.zshrc
-    
-    # make files exec
-    chmod +x ~/.config/hypr/scripts/*
-    
-    # add the Nvidia env file to the config (if needed)
-    if [[ "$ISNVIDIA" == true ]]; then
-        echo -e "\nsource = ~/.config/hypr/nvidia.conf" >> ~/.config/hypr/hyprland.conf
-    fi
-
-    # Copy the SDDM theme
-    echo -e "$CNT - Setting up the login screen."
-    sudo tar -xf src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
-    sudo chown -R $USER:$USER /usr/share/sddm/themes/sugar-candy
-    sudo mkdir /etc/sddm.conf.d
-    echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf.d/10-theme.conf &>> $INSTLOG
-    WLDIR=/usr/share/wayland-sessions
-    if [ -d "$WLDIR" ]; then
-        echo -e "$COK - $WLDIR found"
-    else
-        echo -e "$CWR - $WLDIR NOT found, creating..."
-        sudo mkdir $WLDIR
-    fi 
-    
-    # stage the .desktop file
-    sudo cp src/hyprland.desktop /usr/share/wayland-sessions/
-
-    # add VScode extensions
-    echo -e "$CNT - Adding VScode Extensions"
-    mkdir ~/.vscode
-    tar -xf src/extensions.tar.gz -C ~/.vscode/
-
-    # Font install for Rofi 
-    echo -e "$CNT - Adding Fonts for Rofi"
-    sudo mkdir $HOME/.local/share/fonts
-    sudo cp src/Icomoon-Feather.ttf $HOME/.local/share/fonts
 fi
 
 ### Install the starship shell ###
-read -rep $'[\e[1;33mACTION\e[0m] - Would you like to activate zsh shell? (y,n) ' ZSH
+read -rep $'[\e[1;33mACTION\e[0m] - Would you like to activate zsh? (y,n) ' ZSH
 if [[ $ZSH == "Y" || $ZSH == "y" ]]; then
     # install zsh shell
     echo -e "$CNT - ZSH, Engage!"
     chsh -s $(which zsh)
 fi
 
-### Write files ###
-grep -q "export XDG_CONFIG_HOME=$HOME/.config/" ~/.zshrc ||
-mkdir -p $HOME/.config/ $HOME/.cache/ $HOME/.local/share/ $HOME/.local/state/
-cat << EOF | tee -a ~/.zshrc
-export XDG_CONFIG_HOME=$HOME/.config/
-export XDG_CACHE_HOME=$HOME/.cache/
-export XDG_DATA_HOME=$HOME/.local/share/
-export XDG_STATE_HOME=$HOME/.local/state/
-EOF
+# make files exec
+chmod +x ~/.config/hypr/scripts/*
+
+# stage the .desktop file
+sudo cp src/hyprland.desktop /usr/share/wayland-sessions/
+
+# add VScode extensions
+echo -e "$CNT - Adding VScode Extensions"
+mkdir ~/.vscode
+tar -xf src/extensions.tar.gz -C ~/.vscode/
+
+# Font install for Rofi 
+echo -e "$CNT - Adding Fonts for Rofi"
+sudo mkdir $HOME/.local/share/fonts
+sudo cp src/Icomoon-Feather.ttf $HOME/.local/share/fonts
+
+# Copy the SDDM theme
+echo -e "$CNT - Setting up the login screen."
+sudo tar -xf src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
+sudo chown -R $USER:$USER /usr/share/sddm/themes/sugar-candy
+sudo mkdir /etc/sddm.conf.d
+echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf.d/10-theme.conf &>> $INSTLOG
+WLDIR=/usr/share/wayland-sessions
+
+if [ -d "$WLDIR" ]; then
+    echo -e "$COK - $WLDIR found"
+else
+    echo -e "$CWR - $WLDIR NOT found, creating..."
+    sudo mkdir $WLDIR
+fi
+
+# Start the bluetooth service
+echo -e "$CNT - Starting the Bluetooth Service..."
+sudo systemctl enable --now bluetooth.service &>> $INSTLOG
+sleep 2
+
+# Enable the sddm login manager service
+echo -e "$CNT - Enabling the SDDM Service..."
+sudo systemctl enable sddm &>> $INSTLOG
+sleep 2
+
+# Clean out other portals
+echo -e "$CNT - Cleaning out conflicting xdg portals..."
+yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk &>> $INSTLOG
 
 source $BIN/write.sh
-
 sudo gpasswd -a $USER input
 fc-cache -fv &>> $INSTLOG
 
