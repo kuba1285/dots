6df9361778b91243be763bd9f0deb2fdaf03eab2
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-22 01:09:28 +0900

    Create x-write.sh

---
+++ bin/x-write.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+    grep -q "TearFree" /etc/X11/xorg.conf.d/20-intel.conf ||
+    cat << EOF | sudo tee -a /etc/X11/xorg.conf.d/20-intel.conf
+    GSection "Device"
+    Identifier "Intel Graphics"
+    Driver "intel"
+    Option "TearFree" "true"
+    EndSection
+EOF
+    grep -q "xinput set-prop 11 318 1" ~/.xsessionrc ||
+    touch ~/.xsessionrc
+    cat << EOF | tee -a ~/.xsessionrc
+    xinput set-prop 11 318 1
+    xinput --set-prop "Apple SPI Touchpad" "Coordinate Transformation Matrix" 4 0 0 0 4 0 0 0 1
+EOF
