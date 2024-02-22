859cdef78ef87ff13c2d1d05c7f2a1ebed2141dc
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-02-22 09:13:52 +0900

    Update write.sh

--- bin/write.sh
+++ bin/write.sh
@@ -31,3 +31,21 @@ TryExec=/usr/bin/steam
 Icon=
 Type=Application
 EOF
+
+read -rep $'[\e[1;33mACTION\e[0m] - Would you like to write X11 config? (y,n) ' X11
+if [[ $X11 == "Y" || $X11 == "y" ]]; then
+  grep -q "TearFree" /etc/X11/xorg.conf.d/20-intel.conf ||
+  cat << EOF | sudo tee -a /etc/X11/xorg.conf.d/20-intel.conf
+  GSection "Device"
+  Identifier "Intel Graphics"
+  Driver "intel"
+  Option "TearFree" "true"
+  EndSection
+EOF
+  grep -q "xinput set-prop 11 318 1" ~/.xsessionrc ||
+  touch ~/.xsessionrc
+  cat << EOF | tee -a ~/.xsessionrc
+  xinput set-prop 11 318 1
+  xinput --set-prop "Apple SPI Touchpad" "Coordinate Transformation Matrix" 4 0 0 0 4 0 0 0 1
+EOF
+fi
