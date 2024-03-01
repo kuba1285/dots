571740a297ebb72e7c244504d11fa7592cb96496
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-02 05:39:38 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -157,22 +157,7 @@ fi
 if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia ; then
     echo -e "${CYAN}NOTE${RESET} - Nvidia GPU support setup stage, this may take a while..."
     install_list $LISTNVIDIA
-
-    # update config
-    sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-    sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-    echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
-    echo -e "WLR_NO_HARDWARE_CURSORS=1" | sudo tee -a /etc/environment
-    echo -e "\nsource = ~/.config/hypr/nvidia.conf" >> ~/.config/hypr/hyprland.conf
-
-    # check for hyprland and remove it so the -nvidia package can be installed
-    if yay -Q hyprland &>> /dev/null ; then
-    
-        # Install the correct hyprland version
-        echo -e "${CYAN}NOTE${RESET} - Installing Hyprland, this may take a while..."
-        
-        yay -R --noconfirm hyprland &>> $INSTLOG &
-        install_software hyprland-nvidia
+    source $BIN/nvidia.sh
     fi
 fi
 
