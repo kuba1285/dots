4e8d07b99c22d5d1fd20b11ecc3b0d14616f363f
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-02 05:55:48 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -175,38 +175,6 @@ if [[ $YN = y ]] ; then
     cp -rT $PARENT/. ~/ &>> $INSTLOG
 fi
 
-# make files exec
-chmod +x ~/.config/hypr/scripts/*
-
-# stage the .desktop file
-WLDIR=/usr/share/wayland-sessions
-if [ ! -d "$WLDIR" ] ; then
-    sudo mkdir $WLDIR
-fi 
-sudo cp $PARENT/src/hyprland.desktop $WLDIR
-
-# add VScode extensions
-echo -e "${CYAN}NOTE${RESET} - Adding VScode Extensions"
-mkdir ~/.vscode
-tar -xf $PARENT/src/extensions.tar.gz -C ~/.vscode/
-
-# Copy the SDDM theme
-echo -e "${CYAN}NOTE${RESET} - Setting up the login screen."
-sudo tar -xf $PARENT/src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
-sudo chown -R $USER:$USER /usr/share/sddm/themes/sugar-candy
-sudo mkdir /etc/sddm.conf.d
-echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf.d/10-theme.conf
-
-# Clean out other portals
-echo -e "${CYAN}NOTE${RESET} - Cleaning out conflicting xdg portals..."
-yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk &>> $INSTLOG
-
-# Enable services
-for SERVICE in ${SERVICES[@]} ; do
-    sudo systemctl enable $SERVICE --now &>> $INSTLOG
-    sleep 2
-done
-
 # Install MBP audio driver
 wait_yn "${YELLOW}ACITION${RESET} - Would you like to install MBP audio driver?"
 if [[ $YN = y ]] ; then
@@ -220,7 +188,15 @@ if [[ $YN = y ]] ; then
     show_progress $!
 fi
 
+# Enable services
+for SERVICE in ${SERVICES[@]} ; do
+    sudo systemctl enable $SERVICE --now &>> $INSTLOG
+    sleep 2
+done
+
+source $BIN/stage.sh
 source $BIN/write.sh
+
 sudo gpasswd -a $USER input
 fc-cache -fv &>> $INSTLOG
 
