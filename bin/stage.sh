f57e05a243b12513e70bc9dfc17028847dd93201
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-02 06:27:44 +0900

    Update stage.sh

--- bin/stage.sh
+++ bin/stage.sh
@@ -11,17 +11,14 @@ fi
 sudo cp $PARENT/src/hyprland.desktop $WLDIR
 
 # add VScode extensions
-echo -e "${CYAN}NOTE${RESET} - Adding VScode Extensions"
 mkdir ~/.vscode
 tar -xf $PARENT/src/extensions.tar.gz -C ~/.vscode/
 
 # Copy the SDDM theme
-echo -e "${CYAN}NOTE${RESET} - Setting up the login screen."
 sudo tar -xf $PARENT/src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
 sudo chown -R $USER:$USER /usr/share/sddm/themes/sugar-candy
 sudo mkdir /etc/sddm.conf.d
 echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf.d/10-theme.conf
 
 # Clean out other portals
-echo -e "${CYAN}NOTE${RESET} - Cleaning out conflicting xdg portals..."
 yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk &>> $INSTLOG
