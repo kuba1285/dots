0d51e36074335921482043e5c2699ebd5bba2b4e
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-06 21:47:09 +0900

    Add files via upload

---
+++ .config/polybar/scripts/wallpaper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Directory containing wallpapers
+WALLPAPER_DIR="$HOME/Pictures/"
+
+# File to store the current wallpaper path
+CURRENT_WALLPAPER_FILE="$WALLPAPER_DIR/wallpaper.rofi"
+
+# Get a list of image files in the directory
+WALLPAPERS=("$WALLPAPER_DIR"/*.jpg "$WALLPAPER_DIR"/*.png)
+
+# Find the index of the next wallpaper in the array
+NEXT_INDEX=$(($(($RANDOM % ${#WALLPAPERS[@]})) - 1))
+
+# Set the next wallpaper using feh img with the full path
+feh --bg-center "${WALLPAPERS[$NEXT_INDEX]}" ; wal -i "${WALLPAPERS[$NEXT_INDEX]}" -n
+
+# Copy the currently set wallpaper to current_wallpaper.rofi
+cp "${WALLPAPERS[$NEXT_INDEX]}" "$CURRENT_WALLPAPER_FILE"
+
+# cava
+cp ~/.cache/wal/colors-cava ~/.config/cava/config
+# kitty 
+cp ~/.cache/wal/colors-kitty.conf ~/.config/kitty/
+# mako
+cp ~/.cache/wal/colors-mako ~/.config/mako/config
+ # rofi 
+cp ~/.cache/wal/colors-rofi.rasi ~/.config/rofi/colors/colors-rofi.rasi
+ # firefox
+pywalfox update
