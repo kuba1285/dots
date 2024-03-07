78112870738f6fa459121fd7c5bc1b41f2fcb22a
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-08 06:21:18 +0900

    Update custom.sh

--- bin/custom.sh
+++ bin/custom.sh
@@ -2,11 +2,14 @@
 
 cd
 go install github.com/orangekame3/paclear@latest
+rm -rf go
 
 cd
-git clone http://github.com/possatti/pokemonsay &>> $INSTLOG
+git clone http://github.com/possatti/pokemonsay
 cd pokemonsay
-./install.sh &>> $INSTLOG
+./install.sh
+cd
+rm -rf pokemonsay
 
 cd
 wget https://github.com/autopawn/3d-ascii-viewer/archive/refs/tags/v1.4.0.tar.gz
@@ -15,3 +18,6 @@ cd 3d-ascii-viewer*
 make
 find ./models -name "*.mtl" -type f | xargs rm
 mv 3d-ascii-viewer models /Users/$USER/bin/.
+cd
+rm xvzf v1.4.0.tar.gz
+rm -rf 3d-ascii-viewer*
