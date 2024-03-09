dccb2c2b44ad093e05ea5c196489d105d21c296a
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-09 18:02:52 +0900

    Update custom.sh

--- bin/custom.sh
+++ bin/custom.sh
@@ -5,13 +5,6 @@ go install github.com/orangekame3/paclear@latest
 rm -rf go
 
 cd
-git clone http://github.com/possatti/pokemonsay
-cd pokemonsay
-./install.sh
-cd
-rm -rf pokemonsay
-
-cd
 wget https://github.com/autopawn/3d-ascii-viewer/archive/refs/tags/v1.4.0.tar.gz
 tar xvzf v1.4.0.tar.gz
 cd 3d-ascii-viewer*
